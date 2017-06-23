379fb7db86018b8e78388e6fcaa42036aa77fc94
Jens Janssen <janssen@physik.uni-bonn.de>
2017-06-23 11:41:01 +0200

    ENH: cleanup

--- testbeam_analysis/tools/data_selection.py
+++ testbeam_analysis/tools/data_selection.py
@@ -14,22 +14,16 @@ from numba import njit
 
 from testbeam_analysis.tools import analysis_utils
 
-# Hit data dtype
-hit_dcr = np.dtype([('event_number', np.int64), ('frame', np.uint8),
-                    ('column', np.uint16), ('row', np.uint16),
-                    ('charge', np.uint16)])
 
-
-def combine_hit_files(hit_files, combined_file, event_number_offsets=None,
-                      chunk_size=1000000):
+def combine_hit_files(input_hit_files, output_hit_file=None, event_number_offsets=None, chunk_size=1000000):
     ''' Combine hit files of runs with same parameters to increase statistics.
 
     Parameters
     ----------
-    hit_files : iterable
-        Filenames of files containing the hit array.
-    combined_file : string
-        Filename of the output file containing the combined hit array.
+    input_hit_files : iterable
+        List of filenames of the input hit files containing a hit array.
+    output_hit_file : string
+        File name of the output hit file containing the combined hit array.
     event_number_offsets : iterable
         Manually set start event number offset for each hit array.
         The event number is increased by the given number.
@@ -37,16 +31,15 @@ def combine_hit_files(hit_files, combined_file, event_number_offsets=None,
     chunk_size : int
         Chunk size of the data when reading from file.
     '''
+    if not output_hit_file:
+        prefix = os.path.commonprefix(input_hit_files)
+        output_hit_file = os.path.splitext(prefix)[0] + '_combined.h5'
+
     last_event_number = 0
     used_event_number_offsets = []
-    with tb.open_file(combined_file, mode="w") as out_file:
-        hits_out = out_file.create_table(out_file.root, name='Hits',
-                                         description=hit_dcr,
-                                         title='Selected FE-I4 hits',
-                                         filters=tb.Filters(complib='blosc',
-                                                            complevel=5,
-                                                            fletcher32=False))
-        for index, hit_file in enumerate(hit_files):
+    with tb.open_file(output_hit_file, mode="w") as out_file:
+        hits_out = None
+        for index, hit_file in enumerate(input_hit_files):
             if event_number_offsets and event_number_offsets[index] is not None:
                 event_number_offset = event_number_offsets[index]
             elif index == 0:
@@ -58,142 +51,103 @@ def combine_hit_files(hit_files, combined_file, event_number_offsets=None,
                 for hits, _ in analysis_utils.data_aligned_at_events(
                         in_file_h5.root.Hits, chunk_size=chunk_size):
                     hits[:]['event_number'] += event_number_offset
+                    if hits_out is None:
+                        hits_out = out_file.create_table(where=out_file.root,
+                                                         name='Hits',
+                                                         description=in_file_h5.root.Hits.dtype,
+                                                         title=in_file_h5.root.Hits.title,
+                                                         filters=tb.Filters(complib='blosc',
+                                                                            complevel=5,
+                                                                            fletcher32=False))
                     hits_out.append(hits)
+                    hits_out.flush()
                 last_event_number = hits[-1]['event_number']
                 used_event_number_offsets.append(event_number_offset)
 
     return used_event_number_offsets
 
 
-@njit()
-def _delete_events(data, fraction):
-    result = np.zeros_like(data)
-    index_result = 0
-
-    for index in range(data.shape[0]):
-        if data[index]['event_number'] % fraction == 0:
-            result[index_result] = data[index]
-            index_result += 1
-    return result[:index_result]
-
-
-def reduce_hit_files(hit_files, fraction=10, chunk_size=1000000):
-    ''' Delete a fraction of events to allow faster testing of analysis functions.
+def reduce_events(input_file, max_events, output_file=None, chunk_size=1000000):
+    ''' Reducing the size of a file to a given number of events.
 
     Parameters
     ----------
-    hit_files : iterable
-        Filenames of files containing the hit array.
-    fraction : uint
-        The fraction of leftover events,
-        e.g.: 10 would correspond to n_events = total_events / fraction.
+    input_file : string
+        File name of the input file.
+    output_file : string
+        File name of the output file.
+    max_events : utint
+        Maximum number of radomly selected events.
     chunk_size : int
         Chunk size of the data when reading from file.
     '''
+    if not output_file:
+        output_file = os.path.splitext(input_file)[0] + '_reduced.h5'
 
-    for hit_file in hit_files:
-        with tb.open_file(hit_file, mode='r') as in_file:
-            with tb.open_file(os.path.splitext(hit_file)[0] +
-                              '_reduced.h5', mode="w") as out_file:
-                filters = tb.Filters(complib='blosc', complevel=5,
-                                     fletcher32=False)
-                hits_out = out_file.create_table(out_file.root,
-                                                 name='Hits',
-                                                 description=hit_dcr,
-                                                 title='Selected FE-I4 hits',
-                                                 filters=filters)
-                for hits, _ in analysis_utils.data_aligned_at_events(
-                        in_file.root.Hits, chunk_size=chunk_size):
-                    hits_out.append(_delete_events(hits, fraction))
-
-
-def select_hits(hit_file, max_hits=None, condition=None,
-                hit_flag=None, hit_mask=None, quality_flag=None, quality_mask=None,
-                track_quality=None, track_quality_mask=None,
-                output_file=None, chunk_size=1000000):
-    ''' Function to select a fraction of hits fulfilling a given condition.
-
-    Needed for analysis speed up, when very large runs are used.
-
-    Parameters
-    ----------
-    hit_file : string
-        Filename of the input tracks file.
-    max_hits : uint
-        Number of maximum hits with selection. For data reduction.
-    condition : string
-        A condition that is applied to the hits in numexpr. Only if the
-        expression evaluates to True the hit is taken.
-        E.g.: condition = '(track_quality == 2) & (event_number < 1000)'
-    chunk_size : int
-        Chunk size of the data when reading from file.
-    '''
-
-    with tb.open_file(hit_file, mode='r') as in_file:
-        if not output_file:
-            output_file = os.path.splitext(hit_file)[0] + '_reduced.h5'
+    with tb.open_file(input_file, mode='r') as in_file:
         with tb.open_file(output_file, mode="w") as out_file:
             for node in in_file.root:
-                total_hits = node.shape[0]
+                logging.info('Reducing events for node %s', node.name)
+                total_n_tracks = node.shape[0]
+                total_n_tracks_stored = 0
+                total_n_events_stored = 0
                 widgets = ['', progressbar.Percentage(), ' ',
                            progressbar.Bar(marker='*', left='|', right='|'),
                            ' ', progressbar.AdaptiveETA()]
                 progress_bar = progressbar.ProgressBar(widgets=widgets,
-                                                       maxval=total_hits,
+                                                       maxval=total_n_tracks,
                                                        term_width=80)
                 progress_bar.start()
-                hits_out = out_file.create_table(out_file.root, name=node.name,
-                                                 description=node.dtype,
-                                                 title=node.title,
-                                                 filters=tb.Filters(
-                                                     complib='blosc',
-                                                     complevel=5,
-                                                     fletcher32=False))
-                for hits, index in analysis_utils.data_aligned_at_events(node, chunk_size=chunk_size):
-                    n_hits = hits.shape[0]
-                    sel = np.ones((n_hits,), dtype=np.bool)
-                    if hit_flag is not None:
-                        if hit_mask is None:
-                            hit_mask = hit_flag
-                        sel &= ((hits['hit_flag'] & hit_mask) == hit_flag)
-                    if quality_flag is not None:
-                        if quality_mask is None:
-                            quality_mask = quality_flag
-                        sel &= ((hits['quality_flag'] & quality_mask) == quality_flag)
-                    hits = hits[sel]
-                    if condition:
-                        hits = _select_rows_with_condition(hits, condition)
 
-#                     if track_quality:
-#                         # If no mask is defined select all quality bits
-#                         if not track_quality_mask:
-#                             track_quality_mask = int(0xFFFFFFFF)
-#                         sel = (hits['track_quality'] &
-#                                track_quality_mask) == (track_quality)
-#                         hits = hits[sel]
+                tracks_table_out = out_file.create_table(where=out_file.root,
+                                                         name=node.name,
+                                                         description=node.dtype,
+                                                         title=node.title,
+                                                         filters=tb.Filters(
+                                                             complib='blosc',
+                                                             complevel=5,
+                                                             fletcher32=False))
 
-                    if hits.shape[0] == 0:
-                        logging.warning('No hits selected')
+                for data_chunk, index_chunk in analysis_utils.data_aligned_at_events(node, chunk_size=chunk_size):
+                    n_tracks_chunk = data_chunk.shape[0]
 
-                    # Reduce the number of added hits of this chunk to not
-                    # exeed max_hits
-                    if max_hits:
-                        # Calculate number of hits to add for this chunk
-                        # Fraction of hits to add per chunk
-                        hit_fraction = max_hits / total_hits
-                        sel = np.ceil(np.linspace(0,
-                                                  hits.shape[0],
-                                                  int(hit_fraction * n_hits),
-                                                  endpoint=False)).astype(np.int32)
-                        sel = sel[sel < hits.shape[0]]
-                        hits = hits[sel]
+                    unique_events = np.unique(data_chunk["event_number"])
+                    n_events_chunk = unique_events.shape[0]
 
-                    hits_out.append(hits)
-                    progress_bar.update(index)
+                    if total_n_tracks == index_chunk:  # last chunk, adding all remaining events
+                        select_n_events = max_events - total_n_events_stored
+                    elif total_n_events_stored == 0:  # first chunk
+                        select_n_events = int(round(max_events * (n_tracks_chunk / total_n_tracks)))
+                    else:
+                        # calculate correction of number of selected events
+                        correction = (total_n_tracks - index_chunk)/total_n_tracks * 1 / (((total_n_tracks-last_index_chunk)/total_n_tracks)/((max_events-total_n_events_stored_last)/max_events)) \
+                                     + (index_chunk)/total_n_tracks * 1 / (((last_index_chunk)/total_n_tracks)/((total_n_events_stored_last)/max_events))
+                        select_n_events = int(round(max_events * (n_tracks_chunk / total_n_tracks) * correction))
+                    # do not store more events than in current chunk
+                    select_n_events = min(n_events_chunk, select_n_events)
+                    # do not store more events than given by max_events
+                    select_n_events = min(select_n_events, max_events - total_n_events_stored)
+                    np.random.seed(seed=0)
+                    selected_events = np.random.choice(unique_events, size=select_n_events, replace=False)
+                    store_n_events = selected_events.shape[0]
+                    total_n_events_stored += store_n_events
+                    selected_tracks = np.in1d(data_chunk["event_number"], selected_events)
+                    store_n_tracks = np.count_nonzero(selected_tracks)
+                    total_n_tracks_stored += store_n_tracks
+                    data_chunk = data_chunk[selected_tracks]
+
+                    tracks_table_out.append(data_chunk)
+                    tracks_table_out.flush()
+                    total_n_events_stored_last = total_n_events_stored
+                    total_n_tracks_last = total_n_tracks
+                    last_index_chunk = index_chunk
+                    progress_bar.update(index_chunk)
                 progress_bar.finish()
 
 
 def select_tracks(input_tracks_file, select_duts, output_tracks_file=None, condition=None, max_events=None, duts_hit_selection=None, duts_no_hit_selection=None, duts_quality_selection=None, duts_no_quality_selection=None, chunk_size=1000000):
+    ''' Selecting tracks that are matching the conditions.
+    '''
     if not output_tracks_file:
         output_tracks_file = os.path.splitext(input_tracks_file)[0] + '_selected.h5'
 
@@ -294,12 +248,9 @@ def select_tracks(input_tracks_file, select_duts, output_tracks_file=None, condi
 
     with tb.open_file(input_tracks_file, mode='r') as in_file:
         with tb.open_file(output_tracks_file, mode="w") as out_file:
-            for node in in_file.root:
-                actual_dut = int(re.findall(r'\d+', node.name)[-1])
-                if (select_duts and actual_dut not in select_duts):
-                    continue
-                print "track selection for DUT%d" % actual_dut
-                dut_index = np.where(np.array(select_duts) == actual_dut)[0][0]
+            for dut_index, actual_dut in enumerate(select_duts):
+                node = in_file.get_node(in_file.root, 'Tracks_DUT_%d' % actual_dut)
+                logging.info('Selecting tracks for DUT%d', actual_dut)
 
                 hit_flags = 0
                 hit_mask = 0
@@ -320,7 +271,8 @@ def select_tracks(input_tracks_file, select_duts, output_tracks_file=None, condi
                 print quality_flags, bin(quality_flags)
                 print quality_mask, bin(quality_mask)
 
-                tracks_table_out = out_file.create_table(out_file.root, name=node.name,
+                tracks_table_out = out_file.create_table(where=out_file.root,
+                                                         name=node.name,
                                                          description=node.dtype,
                                                          title=node.title,
                                                          filters=tb.Filters(
@@ -338,7 +290,6 @@ def select_tracks(input_tracks_file, select_duts, output_tracks_file=None, condi
                                                        maxval=total_n_tracks,
                                                        term_width=80)
                 progress_bar.start()
-                read = 0
                 for tracks, index_chunk in analysis_utils.data_aligned_at_events(node, chunk_size=chunk_size):
                     n_tracks_chunk = tracks.shape[0]
 
@@ -352,30 +303,29 @@ def select_tracks(input_tracks_file, select_duts, output_tracks_file=None, condi
                     if condition[dut_index]:
                         tracks = _select_rows_with_condition(tracks, condition[dut_index])
 
-                    read += 1
-#                     if read == 5:
-#                         tracks["event_number"] = 0
-#                     n_tracks_chunk = tracks.shape[0]
-
                     unique_events = np.unique(tracks["event_number"])
                     n_events_chunk = unique_events.shape[0]
 
-                    print "**read**", read, index_chunk, total_n_tracks
                     print "n_events_chunk", n_events_chunk
                     print "n_tracks_chunk", n_tracks_chunk
                     if max_events:
                         if total_n_tracks == index_chunk:  # last chunk, adding all remaining events
                             select_n_events = max_events - total_n_events_stored
                         elif total_n_events_stored == 0:  # first chunk
-                            select_n_events = np.ceil(max_events * (n_tracks_chunk / total_n_tracks))
+                            select_n_events = int(round(max_events * (n_tracks_chunk / total_n_tracks)))
                         else:
                             # calculate correction of number of selected events
-                            correction = 1/(((total_n_tracks-last_index_chunk)/total_n_tracks_last)/((max_events-total_n_events_stored_last)/max_events))
-                            select_n_events = np.ceil(max_events * (n_tracks_chunk / total_n_tracks) * correction)
+                            correction = (total_n_tracks - index_chunk)/total_n_tracks * 1 / (((total_n_tracks-last_index_chunk)/total_n_tracks)/((max_events-total_n_events_stored_last)/max_events)) \
+                                         + (index_chunk)/total_n_tracks * 1 / (((last_index_chunk)/total_n_tracks)/((total_n_events_stored_last)/max_events))
+    #                         select_n_events = np.ceil(n_events_chunk * correction)
+    #                         # calculate correction of number of selected events
+    #                         correction = 1/(((total_n_tracks-last_index_chunk)/total_n_tracks_last)/((max_events-total_n_events_stored_last)/max_events))
+                            select_n_events = int(round(max_events * (n_tracks_chunk / total_n_tracks) * correction))
+                            print "correction", correction
                         # do not store more events than in current chunk
                         select_n_events = min(n_events_chunk, select_n_events)
                         # do not store more events than given by max_events
-                        select_n_events = min(int(select_n_events), max_events - total_n_events_stored)
+                        select_n_events = min(select_n_events, max_events - total_n_events_stored)
                         np.random.seed(seed=0)
                         selected_events = np.random.choice(unique_events, size=select_n_events, replace=False)
                         store_n_events = selected_events.shape[0]
