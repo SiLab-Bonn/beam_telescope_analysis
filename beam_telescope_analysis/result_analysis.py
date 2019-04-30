26f1c8775065ad1213f216bb5511684f5e24a5c3
Jens Janssen <janssen@physik.uni-bonn.de>
2019-04-30 15:28:33 +0200

    BUG: remove deuplicate attribute

--- beam_telescope_analysis/result_analysis.py
+++ beam_telescope_analysis/result_analysis.py
@@ -1324,7 +1324,6 @@ def calculate_efficiency(telescope_configuration, input_tracks_file, select_duts
                 dut_group._v_attrs.plot_range = plot_range
                 dut_group._v_attrs.minimum_track_density = minimum_track_density
                 dut_group._v_attrs.cut_distance = cut_distance
-                dut_group._v_attrs.efficiency = (eff, eff_err_min, eff_err_pl)
                 dut_group._v_attrs.dut_extent = dut_extent
                 dut_group._v_attrs.hist_extent = hist_extent
                 dut_group._v_attrs.efficiency = (eff, eff_err_pl, eff_err_min)
