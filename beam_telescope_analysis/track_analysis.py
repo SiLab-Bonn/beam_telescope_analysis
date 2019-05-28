FIX: fix chi2 calculation for kalman filter

beam_telescope_analysis/track_analysis.py
@@ -1639,7 +1639,8 @@ def _fit_tracks_kalman_loop(track_hits, telescope, select_fit_duts, beam_energy,
     slopes /= slopes_mag[:, :, np.newaxis]
 
     # Sum up all chi2 and divide by number of degrees of freedom
-    chi2 = np.nansum(chi2, axis=1) / np.count_nonzero(~np.isnan(chi2), axis=1)
+    # chi2 = np.nansum(chi2, axis=1) / np.count_nonzero(~np.isnan(chi2), axis=1)
+    chi2 = np.nansum(chi2, axis=1) / (3 * (np.count_nonzero(~np.isnan(chi2[:, select_fit_duts]), axis=1) - 3))
 
     return offsets, slopes, chi2, x_err, y_err
 