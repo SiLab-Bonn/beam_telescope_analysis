BUG: use only fit duts for chi2 calculation

beam_telescope_analysis/track_analysis.py
@@ -1637,9 +1637,12 @@ def _fit_tracks_kalman_loop(track_hits, telescope, select_fit_duts, beam_energy,
     slopes_mag = np.sqrt(np.einsum('ijk,ijk->ij', slopes, slopes))
     slopes /= slopes_mag[:, :, np.newaxis]
 
+    if np.any(chi2[~np.isnan(chi2)] < 0.0):
+        print("WARNING: Not all chi2 values are positive!")
+
     # Sum up all chi2 and divide by number of degrees of freedom. TODO: check why chi2 is sometimes negative.
     # chi2 = np.nansum(chi2, axis=1) / np.count_nonzero(~np.isnan(chi2), axis=1)
-    chi2 = np.nansum(chi2, axis=1) / (3 * (np.count_nonzero(~np.isnan(chi2[:, select_fit_duts]), axis=1) - 3))
+    chi2 = np.nansum(chi2[:, select_fit_duts], axis=1) / (3 * (np.count_nonzero(~np.isnan(chi2[:, select_fit_duts]), axis=1) - 3))
 
     return offsets, slopes, chi2, x_err, y_err
 